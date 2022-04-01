fe5a258e8b1c0899990f1c386581cee25e8e8df2
Daniel Eberli <eberlid@gmail.com>
2022-04-01 16:17:43 +0200

    Add project parser files

---
+++ libs/oxi/oxi_core/ProjectVersion.h
@@ -0,0 +1,14 @@
+#ifndef PROJECTVERSION_H
+#define PROJECTVERSION_H
+
+enum ProjectVersion {
+    //
+    Version0 = 1,
+
+    // automatically derive latest version
+    Last,
+    Latest = Last - 1,
+};
+
+
+#endif // PROJECTVERSION_H
