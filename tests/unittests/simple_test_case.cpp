262e7deb85ab02dc4e54c2931c560638dc349572
Daniel Eberli <eberlid@gmail.com>
2022-04-02 19:15:48 +0200

    Tests starting with fixture 'unit' are automatically run in the pipeline

--- tests/unittests/simple_test_case.cpp
+++ tests/unittests/simple_test_case.cpp
@@ -1,13 +1,11 @@
-#include "midiworker.h"
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
 
 using namespace testing;
 
-TEST(dummy, simple_test_case)
+TEST(unit_dummy, simple_test_case)
 {
     // Empty test for now to ensure it can run.
-    MidiWorker midi_Worker = MidiWorker();
     EXPECT_EQ(1, 1);
     ASSERT_THAT(0, Eq(0));
 }
